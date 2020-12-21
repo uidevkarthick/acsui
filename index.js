0c7dc350843e20a055be2570015219f2efe86951
raj9525 <raj9525@github.com>
2020-12-21 19:00:01 +0530

    final Corrections

--- index.js
+++ index.js
@@ -526,11 +526,9 @@ $(document).ready(function () {
   var _originalSize = $(window).width() + $(window).height();
   $(window).resize(function () {
     if ($(window).width() + $(window).height() != _originalSize) {
-      console.log("keyboard active");
       $(".footer").removeClass("fixed");
       $(".footer").addClass("outofview");
     } else {
-      console.log("keyboard closed");
       $(".footer").addClass("fixed");
       $(".footer").removeClass("outofview");
     }
