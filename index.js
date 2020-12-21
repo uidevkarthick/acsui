bcf3192c9b91ec32569a9107a5d18d40d912d93a
raj9525 <raj9525@github.com>
2020-12-21 11:11:29 +0530

    footer change

--- index.js
+++ index.js
@@ -521,3 +521,16 @@ function msgbox(response = "Response Message") {
     document.getElementById("errorBox").style.justifyContent = "";
   }, 4000);
 }
+
+$(document).ready(function () {
+  var _originalSize = $(window).width() + $(window).height();
+  $(window).resize(function () {
+    if ($(window).width() + $(window).height() != _originalSize) {
+      console.log("keyboard active");
+      $(".footer").removeClass("fixed");
+    } else {
+      console.log("keyboard closed");
+      $(".footer").addClass("fixed");
+    }
+  });
+});
